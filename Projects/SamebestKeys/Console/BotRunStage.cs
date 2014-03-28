57cc12d7ea364af614992fe8f46dee19589cf67b
陳就群 <jiowchern@yahoo.com.tw>
2014-03-28 14:45:02 +0800

    增加bot行為

---
+++ Projects/SamebestKeys/Console/BotRunStage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console
+{
+    class BotRunStage : Regulus.Game.IStage
+    {
+        private Regulus.Project.SamebestKeys.IPlayer _Player;
+        private Regulus.Project.SamebestKeys.IObservedAbility _Observed;
+        public event Action DoneEvent;
+        public BotRunStage(Regulus.Project.SamebestKeys.IPlayer _Player, Regulus.Project.SamebestKeys.IObservedAbility _Observed)
+        {
+            // TODO: Complete member initialization
+            this._Player = _Player;
+            this._Observed = _Observed;
+        }
+        
+
+        
+        void Regulus.Game.IStage.Enter()
+        {
+            _Player.SetSpeed(2);
+            _Player.Walk(Regulus.Utility.Random.Next(0,360));
+            _Observed.ShowActionEvent += _Observed_ShowActionEvent;
+        }
+
+        void _Observed_ShowActionEvent(Regulus.Project.SamebestKeys.Serializable.MoveInfomation obj)
+        {
+            if (obj.ActionStatue == Regulus.Project.SamebestKeys.ActionStatue.Idle && obj.Speed == 0 && DoneEvent != null)
+            {
+                DoneEvent();
+            }
+        }
+
+        void Regulus.Game.IStage.Leave()
+        {
+            _Observed.ShowActionEvent -= _Observed_ShowActionEvent;
+        }
+
+        void Regulus.Game.IStage.Update()
+        {
+            
+        }
+    }
+}
---
+++ Projects/SamebestKeys/Console/BotTalkStage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console
+{
+    class BotTalkStage: Regulus.Game.IStage
+    {
+        private Regulus.Project.SamebestKeys.IPlayer _Player;
+        
+
+        public event Action DoneEvent;
+        public BotTalkStage(Regulus.Project.SamebestKeys.IPlayer _Player)
+        {
+            
+            this._Player = _Player;            
+        }
+
+
+        static string[] _Messages = { 
+                                    "這是測試",
+                                    "公家機關",
+                                    "司法改革",
+                                    "有間客棧",
+                                    "病入膏肓",};
+        void Regulus.Game.IStage.Enter()
+        {
+
+            _Player.Say(_Messages[Regulus.Utility.Random.Next(0, _Messages.Length)]);
+            DoneEvent();
+        }
+
+        void Regulus.Game.IStage.Leave()
+        {
+                     
+        }
+
+        void Regulus.Game.IStage.Update()
+        {
+            
+        }
+    }
+}
