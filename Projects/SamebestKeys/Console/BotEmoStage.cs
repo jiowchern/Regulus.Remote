241fccf4899080a7dc13c7ba23aad287b3afcbc9
jc <jiowchern@gmail.com>
2014-03-29 23:30:34 +0800

    修改map效能問題

--- Projects/SamebestKeys/Console/BotEmoStage.cs
+++ Projects/SamebestKeys/Console/BotEmoStage.cs
@@ -8,24 +8,24 @@ namespace Console
     class BotEmoStage : Regulus.Game.IStage
     {
         private Regulus.Project.SamebestKeys.IPlayer _Player;
-        private Regulus.Project.SamebestKeys.IObservedAbility _Observed;
+        
         Regulus.Utility.TimeCounter _TimeCounter;
 
         float _Timeup;
 
         public event Action DoneEvent;
-        public BotEmoStage(Regulus.Project.SamebestKeys.IPlayer _Player, Regulus.Project.SamebestKeys.IObservedAbility _Observed)
+        public BotEmoStage(Regulus.Project.SamebestKeys.IPlayer _Player)
         {
             _TimeCounter = new Regulus.Utility.TimeCounter();
             this._Player = _Player;
-            this._Observed = _Observed;
+         
         }
 
         void Regulus.Game.IStage.Enter()
         {
             var len = Enum.GetValues(typeof(Regulus.Project.SamebestKeys.ActionStatue)).Length - 1;
             _Player.BodyMovements((Regulus.Project.SamebestKeys.ActionStatue)len);
-            _Timeup = Regulus.Utility.Random.Next(3,30);
+            _Timeup = Regulus.Utility.Random.Next(3,10);
         }
 
         void Regulus.Game.IStage.Leave()
