8d9b03b4499365807419913a3c0b9cf0363be71d
jc <jc@vgame.com.tw>
2015-06-18 17:57:44 +0800

    new version

--- Projects/FishHunter/FormulaWCFLibrary/Properties/AssemblyInfo.cs
+++ Projects/FishHunter/FormulaWCFLibrary/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 預設值，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
