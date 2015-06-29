d563b05542ad5dd1978590cb8634dd61b08a4539
jc <jc@vgame.com.tw>
2015-06-29 13:38:11 +0800

    add score table

---
+++ Projects/FishHunter/CommonTest/ScoreOddsSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using VGame.Project.FishHunter;
+
+namespace GameTest
+{
+    [Binding]
+    public class ScoreOddsSteps
+    {
+        ScoreOddsTable _SorceOddsTable;
+        
+        [Given(@"賠率表")]
+        public void Given賠率表(Table table)
+        {
+            _SorceOddsTable = new ScoreOddsTable(_ToData(table));
+        }
+        
+        [When(@"機率是(.*)")]
+        public void When機率是(float p0)
+        {
+            var value = _SorceOddsTable.Dice(p0);
+            ScenarioContext.Current.Set<int>(value, "ScoreOdds");
+        }
+        
+        [Then(@"賠率為(.*)")]
+        public void Then賠率為(int p0)
+        {
+            var odds = ScenarioContext.Current.Get<int>("ScoreOdds");
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(p0, odds);
+        }
+
+        private VGame.Project.FishHunter.ScoreOddsTable.Data[] _ToData(Table table)
+        {
+            var datas = table.CreateSet<VGame.Project.FishHunter.ScoreOddsTable.Data>();
+            return datas.ToArray();                    
+        }
+    }
+}
