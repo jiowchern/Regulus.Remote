404638ea49966a554aa0f0f821eed5fe28192638
jc <jc@vgame.com.tw>
2015-08-04 10:58:07 +0800

    temp

--- Library/PureLib/Properties/AssemblyInfo.cs
+++ Library/PureLib/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using System.Runtime.InteropServices;
 // 指定為預設值:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.1")]
-[assembly: AssemblyFileVersion("0.0.100.3")]
+[assembly: AssemblyFileVersion("0.0.101.3")]
 [assembly: AssemblyInformationalVersion("0.0.8.3")]
 [assembly: InternalsVisibleTo("RegulusLibraryTest")]
--- Projects/FishHunter/Common/GPIs/IAccountManager.cs
+++ Projects/FishHunter/Common/GPIs/IAccountManager.cs
@@ -17,12 +17,11 @@ using VGame.Project.FishHunter.Common.Datas;
 
 namespace VGame.Project.FishHunter.Common.GPIs
 {
-	public interface IAccountManager
+    public interface IAccountManager : IAccountCreator
 	{
 		Value<Account[]> QueryAllAccount();
 
-		Value<ACCOUNT_REQUEST_RESULT> Create(Account account);
-
+		
 		Value<ACCOUNT_REQUEST_RESULT> Delete(string account);
 
 		Value<ACCOUNT_REQUEST_RESULT> Update(Account account);
