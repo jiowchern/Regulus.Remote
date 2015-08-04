2b18359592165522e54bd7930d1e300abe8377a6
jc <jc@vgame.com.tw>
2015-08-04 11:24:42 +0800

    merge

---
+++ Projects/FishHunter/Common/GPIs/IAccountCreator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VGame.Project.FishHunter.Common.GPIs
+{
+    public interface IAccountCreator
+    {
+        Regulus.Remoting.Value<Common.Datas.ACCOUNT_REQUEST_RESULT> Create(Common.Datas.Account account);
+    }
+}
