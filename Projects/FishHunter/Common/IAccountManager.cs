320bbfad0d6fdfccfd38d8b232f2ae1d2af7ac30
jc <jc@vgame.com.tw>
2015-08-04 10:10:53 +0800

    account creator

---
+++ Projects/FishHunter/Common/IAccountManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VGame.Project.FishHunter
+{
+    public interface IAccountManager : IAccountCreator
+    {
+        Regulus.Remoting.Value<Data.Account[]> QueryAllAccount();
+        
+        Regulus.Remoting.Value<ACCOUNT_REQUEST_RESULT> Delete(string account);
+        Regulus.Remoting.Value<ACCOUNT_REQUEST_RESULT> Update(Data.Account account);        
+    }
+}
