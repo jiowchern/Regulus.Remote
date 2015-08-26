bdfc0822a74298b198fc2abbca9af154f6defb28
jc <jc@vgame.com.tw>
2015-08-26 16:54:12 +0800

    add defference noticer

---
+++ Library/PureLib/Collection/DifferenceNoticer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace Regulus.Collection
+{
+    public class DifferenceNoticer<T>
+    {
+        public delegate void MoveCallback(IEnumerable<T> instances);
+
+        public event MoveCallback JoinEvent;
+        public event MoveCallback LeftEvent;
+        private readonly List<T> _Controllers;
+
+        public DifferenceNoticer()
+        {
+            _Controllers = new List<T>();
+        }
+        public void Set(IEnumerable<T> targets)
+        {
+            var current = _Controllers;
+
+            _BroadcastJoin(targets.Except(current));
+            _BroadcastLeft(current.Except(targets));
+
+            _Controllers.Clear();
+            _Controllers.AddRange(targets);
+        }
+
+        private void _BroadcastLeft(IEnumerable<T> controllers)
+        {
+            if(LeftEvent != null)
+                LeftEvent(controllers);
+        }
+
+        private void _BroadcastJoin(IEnumerable<T> controllers)
+        {
+            if (JoinEvent != null)
+                JoinEvent(controllers);
+        }
+    }
+}
---
+++ Library/PureLibTest/UnitTestDifferenceNoticer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+using Regulus.Collection;
+
+namespace RegulusLibraryTest
+{
+    [TestClass]
+    public class UnitTestDifferenceNoticer
+    {
+        [TestMethod]
+        public void DifferenceNoticerTestSet()
+        {
+            var joinResult = new List<int[]>();
+            var leftResult = new List<int[]>();
+            var before = new int[]
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            var after = new int[]
+            {
+                2,3,4,5,6
+            };
+            var differenceNoticer = new DifferenceNoticer<int>();
+            differenceNoticer.JoinEvent += instances => { joinResult.Add(instances.ToArray()); };
+            differenceNoticer.LeftEvent += instances => { leftResult.Add(instances.ToArray()); };
+
+            differenceNoticer.Set(before); // j0 1 , 2 ,3 ,4 ,5 : l0
+            differenceNoticer.Set(after); // j1 6 : l1 1
+
+            var vertifyJoin = joinResult.ToArray();
+            var vertifyLeft = leftResult.ToArray();
+            Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyJoin[0] , new[] {1,2,3,4,5}));
+            Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyJoin[1], new[] { 6 }));
+            Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyLeft[1], new[] { 1 }));
+        }
+    }
+}
