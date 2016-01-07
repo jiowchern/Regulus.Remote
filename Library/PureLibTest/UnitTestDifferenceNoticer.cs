20cd2d55fc09f82203b314e34eb187886205ee39
jc <jc@vgame.com.tw>
2016-01-07 14:52:34 +0800

    add defference noticer comparer

--- Library/PureLibTest/UnitTestDifferenceNoticer.cs
+++ Library/PureLibTest/UnitTestDifferenceNoticer.cs
@@ -44,5 +44,50 @@ namespace RegulusLibraryTest
             Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyJoin[1], new[] { 6 }));
             Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyLeft[1], new[] { 1 }));
         }
+
+        [TestMethod]
+        public void DifferenceNoticerTestSetWithParam()
+        {
+            var joinResult = new List<int[]>();
+            var leftResult = new List<int[]>();
+            var before = new int[]
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            var after = new int[]
+            {
+                2,3,4,5,6
+            };
+            var differenceNoticer = new DifferenceNoticer<int>( new IntComparer() );
+            differenceNoticer.JoinEvent += instances => { joinResult.Add(instances.ToArray()); };
+            differenceNoticer.LeftEvent += instances => { leftResult.Add(instances.ToArray()); };
+
+            differenceNoticer.Set(before); // j0 1 , 2 ,3 ,4 ,5 : l0
+            differenceNoticer.Set(after); // j1 6 : l1 1
+
+            var vertifyJoin = joinResult.ToArray();
+            var vertifyLeft = leftResult.ToArray();
+            Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyJoin[0], new[] { 1, 2, 3, 4, 5 }));
+            Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyJoin[1], new[] { 6 }));
+            Assert.IsTrue(Regulus.Utility.ValueHelper.DeepEqual(vertifyLeft[1], new[] { 1 }));
+        }
+    }
+
+    public class IntComparer : IEqualityComparer<int>
+    {
+        bool IEqualityComparer<int>.Equals(int x, int y)
+        {
+            return x == y;
+        }
+
+        int IEqualityComparer<int>.GetHashCode(int obj)
+        {
+            return obj.GetHashCode();
+        }
     }
 }
