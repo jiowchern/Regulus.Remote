0227a2cf7a8237f646b3683faf6a8d7fc5d907a0
jc <jc@vgame.com.tw>
2016-01-07 14:52:34 +0800

    add defference noticer comparer

--- Library/PureLib/Collection/DifferenceNoticer.cs
+++ Library/PureLib/Collection/DifferenceNoticer.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 namespace Regulus.Collection
 {
-    public class DifferenceNoticer<T>
+    public class DifferenceNoticer<T> 
     {
         public delegate void MoveCallback(IEnumerable<T> instances);
 
@@ -10,16 +10,40 @@ namespace Regulus.Collection
         public event MoveCallback LeftEvent;
         private readonly List<T> _Controllers;
 
+        private IEqualityComparer<T> _EqualityComparer;
+
+        private System.Action<IEnumerable<T> , IEnumerable<T> > _Broadcast;
+
         public DifferenceNoticer()
         {
             _Controllers = new List<T>();
+            _Broadcast = _DefaultBroadcast;
         }
+
+        private void _DefaultBroadcast(IEnumerable<T> targets , IEnumerable<T> current)
+        {
+            _BroadcastJoin(targets.Except(current));
+            _BroadcastLeft(current.Except(targets));
+        }
+
+        public DifferenceNoticer(IEqualityComparer<T> equality_comparer) :this()
+        {
+            _EqualityComparer = equality_comparer;
+
+            _Broadcast = _EqualityBroadcast;
+        }
+
+        private void _EqualityBroadcast(IEnumerable<T> targets, IEnumerable<T> current)
+        {
+            _BroadcastJoin(targets.Except(current , _EqualityComparer));
+            _BroadcastLeft(current.Except(targets , _EqualityComparer));
+        }
+
         public void Set(IEnumerable<T> targets)
         {
             var current = _Controllers;
 
-            _BroadcastJoin(targets.Except(current));
-            _BroadcastLeft(current.Except(targets));
+            _Broadcast(targets , current);            
 
             _Controllers.Clear();
             _Controllers.AddRange(targets);
@@ -36,5 +60,7 @@ namespace Regulus.Collection
             if (JoinEvent != null)
                 JoinEvent(controllers);
         }
+
+        
     }
 }
