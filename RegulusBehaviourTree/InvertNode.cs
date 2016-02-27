2e9b79bc918519026e2fd6abd498ab5f856d7925
jc <jc@gmail.com>
2016-02-28 07:52:07 +0800

    增加反節點

---
+++ RegulusBehaviourTree/InvertNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Regulus.BehaviourTree
+{
+    internal class InvertNode  : IParent
+    {
+        private ITicker _Ticker;
+
+        public InvertNode()
+        {            
+        }
+
+        TICKRESULT ITicker.Tick(float delta)
+        {
+            var result = _Ticker.Tick(delta);
+            if(result == TICKRESULT.FAILURE)
+                return TICKRESULT.SUCCESS;
+            else if(result == TICKRESULT.SUCCESS)
+                return TICKRESULT.FAILURE;
+            return result;
+        }
+
+        void IParent.Add(ITicker ticker)
+        {
+            if(_Ticker != null)
+                throw new Exception("Inverse nodes only support a single node.");
+            _Ticker = ticker;
+        }
+    }
+}
