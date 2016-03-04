700ddbfcdcbf46c4bb87e908e89537a9e0398721
jiowchern <jiowchern@gmail.com>
2016-03-05 02:18:43 +0800

    增加集合洗牌

---
+++ Library/PureLib/Extension/Linq.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Regulus.Extension
+{
+    public static class Linq
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> set)
+        {
+
+            return set.OrderBy( (d) => Regulus.Utility.Random.Instance.NextDouble());
+        }
+    }
+}
