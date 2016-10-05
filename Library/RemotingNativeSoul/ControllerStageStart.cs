4067cc99b7641b100bcffa01c978c998f65e5654
jc <jc@gmail.com>
2016-10-05 11:07:07 +0800

    remove server app log

--- Library/RemotingNativeSoul/ControllerStageStart.cs
+++ Library/RemotingNativeSoul/ControllerStageStart.cs
@@ -19,19 +19,21 @@ namespace Regulus.Remoting.Soul.Native
 
 		private readonly Console.IViewer _View;
 
-	    private readonly string[] _FirstCommand;
+		private readonly string[] _FirstCommand;
 
-	    public StageStart(Command command, Console.IViewer view ,string[] first_command)
+		public StageStart(Command command, Console.IViewer view ,string[] first_command)
 		{
 			_View = view;
-	        _FirstCommand = first_command;
-	        _Command = command;
+			_FirstCommand = first_command;
+			_Command = command;
 		}
 
 		void IStage.Enter()
 		{
-			_Command.Register<int, string, string>("Launch", _Start);
-			_Command.Register<string>("LaunchIni", _StartIni);
+			
+            _Command.RegisterLambda<StageStart, int ,string ,string>(this , (instance , port , path , assembly) => instance.Launch(port , path , assembly));
+		    _Command.RegisterLambda<StageStart, string>(this , (instance, ini_path ) => instance.LaunchIni(ini_path));
+            
 
 			_View.WriteLine("======Ini file format description=====");
 			_View.WriteLine("Example.");
@@ -42,49 +44,49 @@ namespace Regulus.Remoting.Soul.Native
 			_View.WriteLine("======================================");
 
 
-		    if(_HasFirstCommand())
-		    {
-		        _RunFirstCommand();
-		    }
-		    
+			if(_HasFirstCommand())
+			{
+				_RunFirstCommand();
+			}
+			
 		}
 
-	    private void _RunFirstCommand()
-	    {
-	        var command = _FirstCommand[0];
-	        var args = _FirstCommand.Skip(1).ToArray();
-	        var arg = string.Join(" ", args);
-            _View.WriteLine(string.Format("First Run Command {0} {1}.", command, arg));
-            _Command.Run(
-	            command,
-                args);
-	        
-	    }
+		private void _RunFirstCommand()
+		{
+			var command = _FirstCommand[0];
+			var args = _FirstCommand.Skip(1).ToArray();
+			var arg = string.Join(" ", args);
+			_View.WriteLine(string.Format("First Run Command {0} {1}.", command, arg));
+			_Command.Run(
+				command,
+				args);
+			
+		}
 
-	    private bool _HasFirstCommand()
-	    {
-	        return _FirstCommand.Length > 0;
-	    }
+		private bool _HasFirstCommand()
+		{
+			return _FirstCommand.Length > 0;
+		}
 
-	    /*private void _LoadLibrary(string work_dir)
-        {
-                
-            var files = from f in System.IO.Directory.EnumerateFiles(work_dir, "*.dll", System.IO.SearchOption.AllDirectories) select f;
-            foreach(var file in files)
-            {
-                _View.Write("載入程式庫 " + file + "...");
-                try
-                {
-                    var assembly = System.Reflection.Assembly.LoadFile(file);                        
-                    _View.WriteLine("完成!");
-                }
-                catch (SystemException ex)
-                {
-                    _View.WriteLine("失敗!" + ex.ToString());
-                }
-                    
-            }
-        }*/
+		/*private void _LoadLibrary(string work_dir)
+		{
+				
+			var files = from f in System.IO.Directory.EnumerateFiles(work_dir, "*.dll", System.IO.SearchOption.AllDirectories) select f;
+			foreach(var file in files)
+			{
+				_View.Write("載入程式庫 " + file + "...");
+				try
+				{
+					var assembly = System.Reflection.Assembly.LoadFile(file);                        
+					_View.WriteLine("完成!");
+				}
+				catch (SystemException ex)
+				{
+					_View.WriteLine("失敗!" + ex.ToString());
+				}
+					
+			}
+		}*/
 		void IStage.Leave()
 		{
 			_Command.Unregister("Launch");
@@ -95,7 +97,7 @@ namespace Regulus.Remoting.Soul.Native
 		{
 		}
 
-		private void _StartIni(string path)
+		private void LaunchIni(string path)
 		{
 			var ini = new Ini(File.ReadAllText(path));
 			var port_string = ini.Read("Launch", "port");
@@ -103,22 +105,22 @@ namespace Regulus.Remoting.Soul.Native
 			var dllpath = ini.Read("Launch", "path");
 			var className = ini.Read("Launch", "class");
 
-			_Start(port, dllpath, className);
+			Launch(port, dllpath, className);
 		}
 
-		private void _Start(int port, string path, string class_name)
+		public void Launch(int port, string path, string class_name)
 		{
 			var stream = File.ReadAllBytes(path);
 
 			try
 			{
-                var assembly = Assembly.Load(stream);
-                var instance = assembly.CreateInstance(class_name);
-			    var asm = assembly.GetName();
+				var assembly = Assembly.Load(stream);
+				var instance = assembly.CreateInstance(class_name);
+				var asm = assembly.GetName();
 
-                _View.WriteLine($"Version : {asm.Version.ToString()}");             
-                Log.Instance.WriteInfo($"Assembly Version : {asm.Version.ToString()}");
-                DoneEvent(instance as ICore, port, 0);
+				_View.WriteLine($"Version : {asm.Version.ToString()}");             
+				Log.Instance.WriteInfo($"Assembly Version : {asm.Version.ToString()}");
+				DoneEvent(instance as ICore, port, 0);
 			}
 			catch(SystemException ex)
 			{
