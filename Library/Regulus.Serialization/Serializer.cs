6a2883fe05ce8aaf6a7adb956d028f52ebf24548
jc <jc@gmail.com>
2017-04-20 13:33:37 +0800

    增加 varint跟 zigzag演算

--- Library/Regulus.Serialization/Serializer.cs
+++ Library/Regulus.Serialization/Serializer.cs
@@ -327,6 +327,80 @@ namespace Regulus.Serialization
             var typeInstance = new TypeInstance(provider, instance, instanceInfo.Id);
             return typeInstance;
         }
+
+        public class ZigZag
+        {
+            public static uint Encode(int number)
+            {
+                return (uint)((number << 1) ^ (number >> 31));
+            }
+
+
+            public static ulong Encode(long number)
+            {
+                return (ulong)((number << 1) ^ (number >> 63));
+            }
+
+
+            public static int Decode(uint number)
+            {
+                return (int)(number >> 1) ^ -(int)(number & 1);
+            }
+
+            public static long Decode(ulong number)
+            {
+                return (long)(number >> 1) ^ -(long)(number & 1);
+            }
+        }
+
+        public class Varint
+        {
+            
+
+            public static int NumberToBuffer(byte[] buffer, int offset, ulong value)
+            {
+                int i = 0;
+                while (value >= 0x80)
+                {
+                    buffer[offset + i] = (byte)(value | 0x80);
+                    value >>= 7;
+                    i++;
+                }
+                buffer[offset + i] = (byte)value;
+                return i + 1;
+            }
+
+            public static int GetByteCount(ulong value)
+            {
+                int i;
+                for (i = 0; i < 9 && value >= 0x80; i++, value >>= 7) { }
+                return i + 1;
+            }
+            
+
+            public static int BufferToNumber(byte[] buffer, int offset, out ulong value)
+            {
+                value = 0;
+                int s = 0;
+                for (var i = 0; i < buffer.Length - offset; i++)
+                {
+                    if (buffer[offset + i] < 0x80)
+                    {
+                        if (i > 9 || i == 9 && buffer[offset + i] > 1)
+                        {
+                            value = 0;
+                            return -(i + 1); // overflow
+                        }
+                        value |= (ulong)(buffer[offset + i] << s);
+                        return i + 1;
+                    }
+                    value |= (ulong)(buffer[offset + i] & 0x7f) << s;
+                    s += 7;
+                }
+                value = 0;
+                return 0;
+            }
+        }
     }
 }
 
