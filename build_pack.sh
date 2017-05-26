638bfca2141a60e400a90431efa21713fa9ce8cd
jc <jiowchern@yahoo.com.tw>
2017-05-26 14:49:07 +0800

    add log

--- build_pack.sh
+++ build_pack.sh
@@ -1,7 +1,6 @@
-if [ ! -d "./ReleasePack" ]
-	rm -rf "./ReleasePack"
-fi
+@echo "begin build pack "
 
+@echo "make pack  dir "
 mkdir "./RegulusPack"
 
 mkdir "./RegulusPack/Library"
@@ -13,6 +12,7 @@ mkdir "./RegulusPack/Tool"
 mkdir "./RegulusPack/Tool/Server"
 mkdir "./RegulusPack/Tool/ProtocolBuilder"
 
+@echo "copy pack  dir "
 cp "./Library/RegulusBehaviourTree/bin/Release/*.*" "./RegulusPack/Library/BehaviourTree" 
 
 cp "./Library/RemotingNativeGhost/bin/Release/*.*" "./RegulusPack/Library/Client" 
@@ -22,7 +22,7 @@ cp "./Library/RemotingNativeSoul/bin/Release/*.*" "./RegulusPack/Library/Server"
 cp "./Tool/Server/bin/Release/*.*" "./RegulusPack/Tool/Server" 
 cp "./Tool/Regulus.Remoting.Unity.ProtocolBuilder/bin/Release/*.*" "./RegulusPack/Tool/ProtocolBuilder" 
 
-
+@echo "end build pack "
 
 
 
