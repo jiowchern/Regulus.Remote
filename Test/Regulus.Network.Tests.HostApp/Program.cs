81120b8732ed0399c72c1f079ebec86f3c27b9ee
jc <jc@gmail.com>
2017-06-05 02:17:46 +0800

    begin rudp

---
+++ Test/Regulus.Network.Tests.HostApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Regulus.Utility.WindowConsoleAppliction;
+
+namespace Regulus.Network.Tests.HostApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var console = new HostConsole();
+			console.Run();
+		}
+	}
+}
