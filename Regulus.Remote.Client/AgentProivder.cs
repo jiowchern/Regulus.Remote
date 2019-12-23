437c7cf0a76b3fa4067285d0faae21c3b322f1b5
jc <jiowchern@gmail.com>
2019-12-24 00:29:24 +0800

    新增方法

--- Regulus.Remote.Client/AgentProivder.cs
+++ Regulus.Remote.Client/AgentProivder.cs
@@ -9,7 +9,12 @@ namespace Regulus.Remote.Client
         public static Regulus.Remote.IAgent CreateRudp(System.Reflection.Assembly protocol_assembly)
         {
             
-            var protocol = Regulus.Remote.Protocol.ProtocolProvider.Create(protocol_assembly);
+            var protocol = Regulus.Remote.Protocol.ProtocolProvider.Create(protocol_assembly);            
+            return CreateRudp(protocol);
+        }
+
+        public static Regulus.Remote.IAgent CreateRudp(IProtocol protocol)
+        {
             var client = new Regulus.Network.Rudp.ConnectProvider(new Regulus.Network.Rudp.UdpSocket());
             var agent = new Regulus.Remote.Ghost.Agent(protocol, client);
             return agent;
@@ -17,7 +22,11 @@ namespace Regulus.Remote.Client
 
         public static Regulus.Remote.IAgent CreateTcp(System.Reflection.Assembly protocol_assembly)
         {
-            var protocol = Regulus.Remote.Protocol.ProtocolProvider.Create(protocol_assembly);
+            var protocol = Regulus.Remote.Protocol.ProtocolProvider.Create(protocol_assembly);            
+            return CreateTcp(protocol);
+        }
+        public static Regulus.Remote.IAgent CreateTcp(IProtocol protocol)
+        {
             var client = new Regulus.Network.Tcp.ConnectProvider();
             var agent = new Regulus.Remote.Ghost.Agent(protocol, client);
             return agent;
