2794bdaa37d2aa089bcdc8f96482565e9d820def
jc <jiowchern@gmail.com>
2020-12-28 15:50:24 +0800

    spinwait

--- Regulus.Utility/Utility/AutoPowerRegulator.cs
+++ Regulus.Utility/Utility/AutoPowerRegulator.cs
@@ -6,19 +6,21 @@
         private readonly PowerRegulator _PowerRegulator;
 
         private long _PreviousTicks;
-
+        readonly System.Threading.SpinWait _Spin;
         public AutoPowerRegulator(PowerRegulator power_regulator)
         {
-            _Counter = new TimeCounter();
+            _Spin = new System.Threading.SpinWait();
+            /*_Counter = new TimeCounter();
             _PreviousTicks = _Counter.Ticks;
-            _PowerRegulator = power_regulator;
+            _PowerRegulator = power_regulator;*/
         }
 
         public void Operate()
         {
-            long ticks = _Counter.Ticks;
+            _Spin.SpinOnce();
+            /*long ticks = _Counter.Ticks;
             _PowerRegulator.Operate(ticks - _PreviousTicks);
-            _PreviousTicks = ticks;
+            _PreviousTicks = ticks;*/
         }
     }
 }
