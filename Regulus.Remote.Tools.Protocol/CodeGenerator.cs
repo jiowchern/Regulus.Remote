fdc072927f7ce55269ad572948714afe1d008183
jc <jiowchern@gmail.com>
2021-05-24 06:24:05 +0800

    add build task.

---
+++ Regulus.Remote.Tools.Protocol/CodeGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Framework;
+using System;
+
+namespace Regulus.Remote.Tools.Protocol
+{
+    public class CodeGenerator : Microsoft.Build.Utilities.Task
+    {  
+        public string ProtocolFilesOutputDir { get; set; }
+
+
+
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.Normal , "1 2dddd dddd3");
+            return true;
+        }
+
+
+    }
+}
