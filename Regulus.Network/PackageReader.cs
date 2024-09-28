8e22f1e55b38e4587e7c47d995579581e57e1133
jc <jiowchern@gmail.com>
2024-09-28 20:10:29 +0800

    factor PackageReader.

--- Regulus.Network/PackageReader.cs
+++ Regulus.Network/PackageReader.cs
@@ -1,109 +1,100 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
 namespace Regulus.Network
 {
     public class PackageReader
-    {        
+    {
         private readonly IStreamable _Stream;
-        private Regulus.Memorys.IPool _Pool;
+        private readonly Regulus.Memorys.IPool _Pool;
+
         public struct StreamSegment
         {
             public ArraySegment<byte>[] Packages;
-            public ArraySegment<byte> Unpacket;            
+            public ArraySegment<byte> Unpacket;
         }
+
         public PackageReader(IStreamable stream, Regulus.Memorys.IPool pool)
-        {            
-            this._Stream = stream;
+        {
+            _Stream = stream;
             _Pool = pool;
         }
 
-        public async Task<System.Collections.Generic.List<Regulus.Memorys.Buffer>> Read()
+        public async Task<List<Regulus.Memorys.Buffer>> Read()
         {
-            var buffers = new System.Collections.Generic.List<Regulus.Memorys.Buffer>();
-            
+            var buffers = new List<Regulus.Memorys.Buffer>();
             var remainingBuffer = _Pool.Alloc(0);
+
             while (true)
             {
                 var headBuffer = _Pool.Alloc(8);
-                
-
-                // 把 remainingBuffer 的內容移到 headBuffer
-                for(int i = 0; i < remainingBuffer.Bytes.Count; ++i)
-                {
-                    headBuffer.Bytes.Array[headBuffer.Bytes.Offset + i] = remainingBuffer.Bytes.Array[remainingBuffer.Bytes.Offset + i];
-                }
-                var headReadCount = await _Stream.Receive(headBuffer.Bytes.Array, headBuffer.Bytes.Offset + remainingBuffer.Count, headBuffer.Bytes.Count - remainingBuffer.Count);
+                _CopyBuffer(remainingBuffer.Bytes, headBuffer.Bytes, remainingBuffer.Bytes.Count);
+                var headReadCount = await _ReadFromStream(headBuffer.Bytes.Array, headBuffer.Bytes.Offset + remainingBuffer.Count, headBuffer.Bytes.Count - remainingBuffer.Count);
                 headReadCount += remainingBuffer.Bytes.Count;
                 remainingBuffer = _Pool.Alloc(0);
-                if (headReadCount == 0)
-                {
-                    return buffers;
-                }
-                
-                var headReadedBuffer = new ArraySegment<byte>(headBuffer.Bytes.Array, headBuffer.Bytes.Offset, headReadCount);                
-                var headVarint = Regulus.Serialization.Varint.FindVarint(ref headReadedBuffer);
-                if (headVarint.Count == 0)
-                {                    
-                    throw new SystemException("head size greater than 8.");
-                }
-                
-                
-                
-                var bodyVarintSize = Regulus.Serialization.Varint.BufferToNumber(headBuffer.Bytes.Array, headBuffer.Bytes.Offset, out int bodySize);
-                var remaining = headReadCount - bodyVarintSize; // 多讀出來的數量
-                var needReadSize = bodySize - remaining;
-                if(needReadSize <= 0)
-                {
-                    // 不需要再讀取已經可以組出body
-                    var bodyBuffer = _Pool.Alloc(bodySize);
-                    
-                    for(int i = 0; i < remaining + needReadSize; ++i)
-                    {
-                        bodyBuffer.Bytes.Array[bodyBuffer.Bytes.Offset + i] = headBuffer.Bytes.Array[headBuffer.Bytes.Offset + bodyVarintSize  + i];
-                    }
-                    if(bodySize>0)
-                        buffers.Add(bodyBuffer);
-                    if(needReadSize == 0)
-                    {
-                        return buffers;
-                    }
 
-                    // 將剩下的存到remainingBuffer
+                if (headReadCount == 0)
+                    return buffers;
+
+                var headReadedBuffer = new ArraySegment<byte>(headBuffer.Bytes.Array, headBuffer.Bytes.Offset, headReadCount);
+                var headVarint = Regulus.Serialization.Varint.FindVarint(ref headReadedBuffer);
+
+                if (headVarint.Count == 0)
+                    throw new SystemException("head size greater than 8.");
+
+                var bodyVarintSize = Regulus.Serialization.Varint.BufferToNumber(headBuffer.Bytes.Array, headBuffer.Bytes.Offset, out int bodySize);
+                var remaining = headReadCount - bodyVarintSize;
+                var needReadSize = bodySize - remaining;
+
+                if (needReadSize <= 0)
+                {
+                    var bodyBuffer = _Pool.Alloc(bodySize);
+                    _CopyBuffer(headBuffer.Bytes, bodyVarintSize, bodyBuffer.Bytes, 0, bodySize);
+
+                    if (bodySize > 0)
+                        buffers.Add(bodyBuffer);
+
+                    if (needReadSize == 0)
+                        return buffers;
+
                     remainingBuffer = _Pool.Alloc(-needReadSize);
-                    for (int i = 0; i < -needReadSize; ++i)
-                    {
-                        remainingBuffer.Bytes.Array[remainingBuffer.Bytes.Offset + i] = headBuffer.Bytes.Array[headBuffer.Bytes.Offset + bodyVarintSize + bodySize + i];
-                    }
+                    _CopyBuffer(headBuffer.Bytes, bodyVarintSize + bodySize, remainingBuffer.Bytes, 0, -needReadSize);
                     continue;
                 }
                 else
                 {
-                    // 需要再讀取
                     var bodyBuffer = _Pool.Alloc(bodySize);
-                    
-                    var bodyReadCount = 0;
+                    _CopyBuffer(headBuffer.Bytes, headReadCount - remaining, bodyBuffer.Bytes, 0, remaining);
 
-                    // 讀取剩下的body
-                    while(bodyReadCount < needReadSize)
+                    var bodyReadCount = 0;
+                    while (bodyReadCount < needReadSize)
                     {
-                        bodyReadCount += await _Stream.Receive(bodyBuffer.Bytes.Array, bodyBuffer.Bytes.Offset + remaining + bodyReadCount, bodyBuffer.Bytes.Count - remaining - bodyReadCount);                        
+                        bodyReadCount += await _ReadFromStream(bodyBuffer.Bytes.Array, bodyBuffer.Bytes.Offset + remaining + bodyReadCount, needReadSize - bodyReadCount);
                     }
-                    
-                    // 將head多讀取出來的部分放到body
-                    for(int i = 0; i < remaining; ++i)
-                    {
-                        var read = headBuffer.Bytes.Array[headBuffer.Bytes.Offset + headBuffer.Bytes.Count - remaining + i];
-                        bodyBuffer.Bytes.Array[bodyBuffer.Bytes.Offset + i] = read;
-                    }
+
                     if (bodySize > 0)
                         buffers.Add(bodyBuffer);
-                   
+
                     return buffers;
                 }
-
             }
+        }
 
+        private void _CopyBuffer(ArraySegment<byte> source, ArraySegment<byte> destination, int count)
+        {
+            Array.Copy(source.Array, source.Offset, destination.Array, destination.Offset, count);
+        }
+
+        private void _CopyBuffer(ArraySegment<byte> source, int sourceOffset, ArraySegment<byte> destination, int destOffset, int count)
+        {
+            Array.Copy(source.Array, source.Offset + sourceOffset, destination.Array, destination.Offset + destOffset, count);
+        }
+
+        private async Task<int> _ReadFromStream(byte[] buffer, int offset, int count)
+        {
+            return await _Stream.Receive(buffer, offset, count);
         }
     }
 }
